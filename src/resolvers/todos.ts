0ad7ec212cf26c69ba24aedd2581c834605aef78
hiroki_iizuka <alljapan.ma.kabu@gmail.com>
2020-09-11 18:21:37 +0900

    返り値の型

--- src/resolvers/todos.ts
+++ src/resolvers/todos.ts
@@ -6,14 +6,13 @@ type Todo = {
   done: boolean
 }
 
-export const allTodos = (parent: any, args: any, context: any) =>
+export const allTodos = (parent: any, args: any, context: any): Todo[] =>
   context.db.collection(`todos`).find().toArray()
 
-export const addTodo = (parent: any, args: any, context: any) => {
+export const addTodo = (parent: any, args: any, context: any): Todo => {
   const todo = { ...args, id: uuidv4(), done: false }
   context.db.collection(`todos`).insertOne(todo, (err: any, res: any) => {
     if (err) throw err
-    console.log('1 document inserted')
   })
   return todo
 }
