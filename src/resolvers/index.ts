7c2df0ef90f88ebfd9fe8f421790d2e2260e118d
hiroki_iizuka <alljapan.ma.kabu@gmail.com>
2020-09-11 22:33:22 +0900

    構造を変えてIResolversを使う

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,10 +1,18 @@
-import { allTodos, addTodo } from './todos'
+import { IResolvers } from 'graphql-tools'
+import { v4 as uuidv4 } from 'uuid'
 
-export const resolvers = {
+export const resolvers: IResolvers = {
   Query: {
-    allTodos,
+    allTodos: (_parent, _args, context) =>
+      context.db.collection(`todos`).find().toArray(),
   },
   Mutation: {
-    addTodo,
+    addTodo: (_parent, args, context) => {
+      const todo = { ...args, id: uuidv4(), done: false }
+      context.db.collection(`todos`).insertOne(todo, (err: any, res: any) => {
+        if (err) throw err
+      })
+      return todo
+    },
   },
 }
