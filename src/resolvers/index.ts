0ed40042cd0766a2fd8b45089d0ba46986f59c5b
hiroki_iizuka <alljapan.ma.kabu@gmail.com>
2020-09-15 16:09:23 +0900

    作成日付を登録し登録順にソートする

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -5,16 +5,23 @@ type Todo = {
   id: string
   title: string
   done: boolean
+  createdDate: Date
 }
 
 export const resolvers: IResolvers = {
   Query: {
     allTodos: (_parent, _args, context) =>
-      context.db.collection(`todos`).find().toArray(),
+      context.db.collection(`todos`).find().sort({ createdDate: -1 }).toArray(),
   },
   Mutation: {
     addTodo: (_parent, args, context) => {
-      const todo = { ...args, id: uuidv4(), done: false }
+      const date = new Date()
+      const todo: Todo = {
+        ...args,
+        id: uuidv4(),
+        done: false,
+        createdDate: date.toLocaleString(),
+      }
       context.db.collection(`todos`).insertOne(todo, (err: any, res: any) => {
         if (err) throw err
       })
@@ -34,7 +41,7 @@ export const resolvers: IResolvers = {
             if (err) throw err
           },
         )
-      const changedTodo = { ...args, title: todo[0].title, done: true }
+      const changedTodo: Todo = { ...args, ...todo[0], done: true }
       return changedTodo
     },
     deleteTodo: async (_parent, args, context) => {
@@ -47,7 +54,7 @@ export const resolvers: IResolvers = {
         .deleteOne({ id: args.id }, (err: any, _result: any) => {
           if (err) throw err
         })
-      const deletedTodo = { ...args, title: todo[0].title, done: todo[0].done }
+      const deletedTodo: Todo = { ...args, ...todo[0] }
       return deletedTodo
     },
   },
